6453f74f016d1679d802e7a5c9c91c59d542e402
Alessandra <aleterre10@gmail.com>
2021-03-31 22:27:56 -0300

    Created customer-service module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { config as databaseConfig } from 'src/config/database.config';
 
 import { UserModule } from './user/user.module';
+import { CustomerServiceModule } from './customer-service/customer-service.module';
 
 @Module({
   imports: [
     UserModule,
     GraphQLModule.forRoot({ autoSchemaFile: true }),
     TypeOrmModule.forRoot(databaseConfig),
+    CustomerServiceModule,
   ],
 })
 export class AppModule {}
---
+++ src/modules/customer-service/customer-service.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Attendance } from './entities/attendance.entity';
+import { Service } from './entities/service.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Attendance, Service])
+  ]
+})
+export class CustomerServiceModule {}
